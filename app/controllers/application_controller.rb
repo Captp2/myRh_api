fdb09c1131e305fe5b7c21b9e13028498e050095
captp <axel@manager.one>
2018-07-17 17:05:06 +0200

    Auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,25 @@
 class ApplicationController < ActionController::API
 	def current_user
-		@current_user ||= User.find(session[:user_id]) if session[:user_id]
+		user ||= User.find(request.headers[:ID]) if request.headers[:ID]
+		if(user && user.token === request.headers[:token])
+			@current_user = user
+		end
 	end
-	helper_method :current_user
-	before_action :verify_logged_in
+
+	def logged_in?
+		current_user != nil
+	end
 
 	def verify_logged_in
-		# render json: session
-		# if !session[:user_id]
-		# 	response.set_header('role', 'guest')
-		# else
-		# 	@current_user ||= User.find(session[:user_id]) if session[:user_id]
-		# 	response.set_header('role', 'user')
-		# end
+		if logged_in?
+			response.set_header('role', 'user')
+		else
+			response.set_header('role', 'guest')
+		end
 	end
+
+	helper_method :current_user, :logged_in
+	after_action :verify_logged_in
 
 	def authorize
 		redirect_to '/login' unless current_user
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,14 +6,17 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
+      user.generate_token()
+      user.save
       response.set_header('role', 'user');
-      render json: session
+      render json: user
     else
       render json: {"error": 402, "message": "Bad authentication"}
     end
   end
 
   def destroy
+    @current_user.token = nil
+    @current_user.save
   end
 end
