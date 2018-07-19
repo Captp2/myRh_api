78dc311c76dd0e826f4c16f30cbaa94a326f6154
Estelle Barna <estelle.barna@epitech.eu>
2018-07-19 14:26:08 +0200

    crud

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,4 +1,5 @@
 class CompaniesController < ApplicationController
+  ##skip_before_filter :verify_authenticity_token, :only => :create
   before_action :set_company, only: [:show, :edit, :update, :destroy]
 
   # GET /companies
